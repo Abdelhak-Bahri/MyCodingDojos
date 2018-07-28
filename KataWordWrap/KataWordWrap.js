b8db9af9c10efc30c349156c72ad9ce31becf466
Abdelhak-Bahri <ca_bahri@esi.dz>
2018-07-28 10:18:36 +0100

    solution for kataWordWrap on javascript using regex

---
+++ KataWordWrap/KataWordWrap.js
@@ -0,0 +1,17 @@
+/**
+ * Desc: a simple solution for WordWrap Kata 
+ * Link: https://codingdojo.org/kata/WordWrap/
+ * Author: Abdelhak-Bahri
+ * Usage: node KataWordWrap.js maxLen:number "String to wrap"
+ * Example: 
+ * node KataWordWrap/KataWordWrap.js 10  "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker"
+ */
+
+maxLen = process.argv[2];
+oldString = process.argv[3];
+
+wrap = function(oldString, maxLen){
+    return oldString.match(new RegExp('.{1,' + maxLen + '}', 'g')).join('\n');
+};
+
+console.log(wrap(oldString, maxLen));
